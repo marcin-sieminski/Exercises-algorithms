bea37f58de629b0c0daa0fe93053e283553f95fb
Marcin Sieminski <marcin.sieminski@outlook.com>
2021-07-01 20:01:59 +0200

    Add project files.

---
+++ P01BinaryGap.Tests/BinaryGapTests.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace P01BinaryGap.Tests
+{
+    [TestClass]
+    public class BinaryGapTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            
+        }
+    }
+}
---
+++ P01BinaryGap/BinaryGapHelper.cs
@@ -0,0 +1,10 @@
+﻿namespace P01BinaryGap
+{
+    public class BinaryGapHelper
+    {
+        public int BinaryGap(int number)
+        {
+            return 0;
+        }
+    }
+}
