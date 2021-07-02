05ccd1ed1e7e7ed85a4e05ad297a1eeead151b56
Marcin Sieminski <marcin.sieminski@outlook.com>
2021-07-02 21:44:22 +0200

    Add Binary Gap solution

--- P01BinaryGap.Tests/BinaryGapTests.cs
+++ P01BinaryGap.Tests/BinaryGapTests.cs
@@ -6,9 +6,19 @@ namespace P01BinaryGap.Tests
     public class BinaryGapTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void ReturnsProperBinaryGapNumberWhenIntegerNumberInput()
         {
+            var binaryGapHelper = new BinaryGapHelper();
             
+            var number1 = 1054;
+            var number2 = 100;
+            var number3 = 64;
+            var number4 = 14512;
+
+            Assert.AreEqual(5, binaryGapHelper.BinaryGap(number1));
+            Assert.AreEqual(2, binaryGapHelper.BinaryGap(number2));
+            Assert.AreEqual(0, binaryGapHelper.BinaryGap(number3));
+            Assert.AreEqual(3, binaryGapHelper.BinaryGap(number4));
         }
     }
 }
--- P01BinaryGap/BinaryGapHelper.cs
+++ P01BinaryGap/BinaryGapHelper.cs
@@ -1,10 +1,21 @@
-﻿namespace P01BinaryGap
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace P01BinaryGap
 {
     public class BinaryGapHelper
     {
         public int BinaryGap(int number)
         {
-            return 0;
+            var rxGap = new Regex("(?<=1)(0+)(?=1)");
+            var strGap = Convert.ToString(number, 2);
+ 
+            return
+                rxGap.Matches(strGap)
+                    .Select(m => m.Length)
+                    .DefaultIfEmpty(0)
+                    .Max();
         }
     }
 }
